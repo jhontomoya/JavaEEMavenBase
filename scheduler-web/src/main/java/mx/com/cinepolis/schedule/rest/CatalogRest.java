e7cbd9f5453363445dff182b4e2c1041f8ac14ca
jhontomoya <jhon_dasher@hotmail.com>
2018-05-02 19:24:32 -0500

    WebLogic

--- scheduler-web/src/main/java/mx/com/cinepolis/schedule/rest/CatalogRest.java
+++ scheduler-web/src/main/java/mx/com/cinepolis/schedule/rest/CatalogRest.java
@@ -1,12 +1,17 @@
 package mx.com.cinepolis.schedule.rest;
 
+import mx.com.cinepolis.scheduler.commons.to.CatalogsTO;
 import mx.com.cinepolis.scheduler.commons.to.UserTO;
 import mx.com.cinepolis.scheduler.facade.CatalogFacadeEJB;
+
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.Response;
 
 /**
@@ -27,5 +32,17 @@ public class CatalogRest {
         UserTO userTO = catalogFacadeEJB.getSimpleUser();
         return Response.ok().entity(userTO).build();
     }
+    
+    @GET
+    @Produces("application/json")
+    @Path("/country/{pais}")
+    public Response getEstados(@PathParam("pais") String pais) {
+    	List<CatalogsTO> catalogsTOLits =  catalogFacadeEJB.getEstadosList(pais);
+    	
+    	GenericEntity<List<CatalogsTO>> entity = new GenericEntity<List<CatalogsTO>>(catalogsTOLits) {};
+                
+    	return Response.ok().entity(entity).build();
+    }
+    
 
 }
